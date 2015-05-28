985387b5976586fed01ed83db690d20a91cfea76
Shoeb Rehman <rehmanbshoeb@gmail.com>
2015-05-28 18:03:22 -0400

    used Convert.To instead of Parse after learning the difference

--- hello.cs
+++ hello.cs
@@ -10,7 +10,7 @@ namespace AreaApplication
 			  
               Console.WriteLine("Please input the length of the rectangle");
               do{
-                  length = double.Parse(Console.ReadLine());
+                  length = Convert.ToDouble(Console.ReadLine());
                   if (length < 0)
                   {
                       Console.WriteLine("Your input is a non-positive integer. Please try again.");
@@ -20,7 +20,7 @@ namespace AreaApplication
 			  Console.WriteLine("Please input the width of the rectangle");
               do
               {
-                  width = double.Parse(Console.ReadLine());
+                  width = Convert.ToDouble(Console.ReadLine());
                   if (width < 0)
                   {
                       Console.WriteLine("Your input is a non-positive integer. Please try again.");
@@ -28,6 +28,13 @@ namespace AreaApplication
               } while (width < 0);
 			  return;
 		  }
+
+          public void setDimensions(double l, double w)
+          {
+              length = l;
+              width = w;
+          }
+
 		  public double returnArea(){
 			  return length*width;
 			  }
@@ -41,29 +48,28 @@ namespace AreaApplication
 			length = l;
 			width = w;
 		}
-		public Rectangle(){}
+		public Rectangle(){}//default constructor
 	}
 	
 	class ExecuteProgram{
 		static void Main(string[] args){
-			Console.WriteLine("Welcome to my program!\nWould you like to use the constructor or no?");
-			char ans;
+			double ans;
 
-			ans = char.Parse(Console.ReadLine());
-			
-			if(ans != 'y'){
-				//Console.WriteLine("Calls function to receive dimensions for area calculation");
-				Rectangle r = new Rectangle();
-				r.getDimensions();
-				r.DisplayValues();
-			}
-			if(ans == 'y'){
+            Console.WriteLine("Welcome! Please pick one of the following:\n1. Use Constructor\n2. Use Set Dimensions Function\n3. Use Get Dimensions Function\n4. Exit\n");
+            do{
+                ans = Convert.ToInt32(Console.ReadLine());
+                if(ans != 1 && ans != 2 && ans != 3 && ans != 4){
+                    Console.WriteLine("Please enter a valid choice!");
+                }
+            } while(ans != 1 && ans != 2 && ans != 3 && ans != 4);
+
+			if(ans == 1){
 				//Console.WriteLine("Receives user input and then initializes class object through constructor");
 				double l, w;
                 Console.WriteLine("Please enter the Length");
                 do
                 {
-                    l = double.Parse(Console.ReadLine());
+                    l = Convert.ToDouble(Console.ReadLine());
                     if (l < 0)
                     {
                         Console.WriteLine("Your input is a non-positive integer. Please try again.");
@@ -72,7 +78,7 @@ namespace AreaApplication
 				Console.WriteLine("Please enter the Width");
                 do
                 {
-                    w = double.Parse(Console.ReadLine());
+                    w = Convert.ToDouble(Console.ReadLine());
                     if (w < 0)
                     {
                         Console.WriteLine("Your input is a non-positive integer. Please try again.");
@@ -81,7 +87,43 @@ namespace AreaApplication
 				Rectangle f = new Rectangle(l, w);
 				f.DisplayValues();
 			}
+            if(ans == 2){
+                Rectangle r = new Rectangle();
+                double l, w;
+                Console.WriteLine("Please enter the Length");
+                do
+                {
+                    l = Convert.ToDouble(Console.ReadLine());
+                    if (l < 0)
+                    {
+                        Console.WriteLine("Your input is a non-positive integer. Please try again.");
+                    }
+                } while (l < 0);
+                Console.WriteLine("Please enter the Width");
+                do
+                {
+                    w = Convert.ToDouble(Console.ReadLine());
+                    if (w < 0)
+                    {
+                        Console.WriteLine("Your input is a non-positive integer. Please try again.");
+                    }
+                } while (w < 0);
+                r.setDimensions(l, w);
+                r.DisplayValues();
+            }
+            if(ans == 3){
+				//Console.WriteLine("Calls function to receive dimensions for area calculation");
+				Rectangle r = new Rectangle();
+				r.getDimensions();
+				r.DisplayValues();
+			}
+
+            if (ans == 4)
+            {
+                return;
+            }
             Console.ReadLine();
+            return;
         }
 	}
 			
